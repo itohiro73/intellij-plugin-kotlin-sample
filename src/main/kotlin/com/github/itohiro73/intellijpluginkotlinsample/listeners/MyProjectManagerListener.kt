771cfd72ea85d708259c28daec0a4acd059da221
GitHub Action <action@github.com>
2022-12-11 03:13:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/itohiro73/intellijpluginkotlinsample/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/itohiro73/intellijpluginkotlinsample/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.itohiro73.intellijpluginkotlinsample.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.itohiro73.intellijpluginkotlinsample.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/itohiro73/intellijpluginkotlinsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/itohiro73/intellijpluginkotlinsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.itohiro73.intellijpluginkotlinsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.itohiro73.intellijpluginkotlinsample.MyBundle
 
 class MyApplicationService {
 
